17a69dc85ad38a7c9a9fe1113621823d8c75d5ba
pweinsto <37242263+pweinstock@users.noreply.github.com>
2024-10-29 17:55:00 +0100

    initial push thread_safe_iostream.hpp

---
+++ thread_safe_iostream.hpp
@@ -0,0 +1,64 @@
+#ifndef THREAD_SAFE_IOSTREAM_HPP
+# define THREAD_SAFE_IOSTREAM_HPP
+
+# include <iostream>
+# include <mutex>
+# include <string>
+# include <sstream>
+
+class ThreadSafeIOStream
+{
+public:
+	template <typename T>
+	ThreadSafeIOStream& operator<<(const T& value)
+	{
+		m_buffer << value;
+		return *this;
+	}
+
+	ThreadSafeIOStream& operator<<(std::ostream& (*manip)(std::ostream&))
+	{
+		if (manip == static_cast<std::ostream& (*)(std::ostream&)>(std::endl))
+		{
+			flushWithPrefix();
+		}
+		else
+		{
+			m_buffer << manip;
+		}
+		return *this;
+	}
+
+	template <typename T>
+	ThreadSafeIOStream& operator>>(T& value)
+	{
+		std::lock_guard<std::mutex> lock(m_mutex);
+		std::cin >> value;
+		return *this;
+	}
+
+	void setPrefix(const std::string& prefix)
+	{
+		m_prefix = prefix;
+	}
+
+private:
+	void flushWithPrefix()
+	{
+		std::lock_guard<std::mutex> lock(m_mutex);
+		std::cout << m_prefix << m_buffer.str() << std::endl;
+		m_buffer.str("");
+		m_buffer.clear();
+	}
+	
+	std::ostringstream m_buffer;
+	std::string m_prefix;
+	static std::mutex m_mutex;
+
+};
+
+std::mutex ThreadSafeIOStream::m_mutex;
+
+thread_local ThreadSafeIOStream threadSafeCout;
+
+# endif
